73507856b5796f7ed4f2be469677467bbcadc681
Egor Blinov <egor.b@miro.com>
2023-08-21 19:25:33 +0200

    Initial Commit

---
+++ Daily budget/Daily_budgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_budgetApp.swift
+//  Daily budget
+//
+//  Created by Egor Blinov on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Daily_budgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
